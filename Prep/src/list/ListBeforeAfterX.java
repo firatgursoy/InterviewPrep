8f787461d3d209f43f5cf8f554af7ce647e61184
echow23 <echow23@yahoo-inc.com>
2015-09-10 11:50:28 -0400

    add solution for finding a list pivoted around a value

---
+++ Prep/src/list/ListBeforeAfterX.java
@@ -0,0 +1,46 @@
+package list;
+
+public class ListBeforeAfterX {
+
+    private class Node
+    {
+        int data;
+        Node next;
+    };
+
+    public static Node pivot(Node node, int num) {
+        Node before = null;
+        Node after = null;
+        
+        while(node != null) {
+            Node next = node.next;
+            
+            if (node.data < num) {
+                node.next = before;
+                before = node;
+            } else {
+                node.next = after;
+                after = node;
+            }
+
+            node = next;
+        }
+        
+        if (before == null) {
+            return after;
+        }
+        
+        Node head = before;
+        while(before.next != null) {
+            before = before.next;
+        }
+        
+        before.next = after;
+        
+        return head;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
